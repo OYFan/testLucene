82df2d2f2c508e30b2eb2d1b48d5f40599763a3a
oyangfan <1422182412@qq.com>
2018-07-13 09:00:11 +0800

    空白Lucene测试第一次提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by 10170 on 2018/7/11.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,5 @@
+/**
+ * Created by 10170 on 2018/7/11.
+ */
+public class User {
+}
